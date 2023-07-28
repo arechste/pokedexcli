d7fbb8b7748e923d123e66b028a71317d8fa18d2
Alex Rechsteiner <769693+arechste@users.noreply.github.com>
2023-07-28 09:12:52 +0200

    complete cache add get reap reaploop  implementation + tests

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,6 +1,8 @@
 package pokecache
 
-import "time"
+import (
+	"time"
+)
 
 type Cache struct {
 	cache map[string]cacheEntry
@@ -11,10 +13,12 @@ type cacheEntry struct {
 	createdAt time.Time
 }
 
-func NewCache() Cache {
-	return Cache{
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
 		cache: make(map[string]cacheEntry),
 	}
+	go c.reapLoop(interval)
+	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
@@ -27,4 +31,19 @@ func (c *Cache) Add(key string, val []byte) {
 func (c *Cache) Get(key string) ([]byte, bool) {
 	ce, ok := c.cache[key]
 	return ce.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(interval)
+	}
+}
+func (c *Cache) reap(interval time.Duration) {
+	timeAgo := time.Now().UTC().Add(-interval)
+	for k, v := range c.cache {
+		if v.createdAt.Before(timeAgo) {
+			delete(c.cache, k)
+		}
+	}
 }
